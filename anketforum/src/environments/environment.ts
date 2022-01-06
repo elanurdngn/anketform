3d98305fdba418978c2203f353be32b2561181f7
elanurdngn <elanurdngn@gmail.com>
2022-01-06 19:36:39 +0300

    first commit

---
+++ anketforum/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDG8i8K-YqkULgIVOnhhqVd4p7DIa7KCM8",
+    authDomain: "anket-55e54.firebaseapp.com",
+    databaseURL: "https://anket-55e54-default-rtdb.firebaseio.com",
+    projectId: "anket-55e54",
+    storageBucket: "anket-55e54.appspot.com",
+    messagingSenderId: "321212506565",
+    appId: "1:321212506565:web:161aece8736e30eda9abac",
+    measurementId: "G-QRBPCQY98V"
+    }
+};
